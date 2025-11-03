512549fa17d21f5678840cc9af36eecf83024ba3
xosadmin <9164356+xosadmin@users.noreply.github.com>
2025-11-03 11:14:57 +0800

    Introduce yaml support (Experimental)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os,sys
-import configparser
 import subprocess
 import ipaddress
 import time
+import yaml
+import argparse
 
 def testip(inputvalue):
+    if not inputvalue:
+        return False
     if "/" in inputvalue:
         value = inputvalue.split("/")[0]
     else:
@@ -31,27 +34,42 @@ def checkvalue(type,value):
             return True
     return False
 
+def readConf(file):
+    if not os.path.exists(file):
+        return None
+    f = open(file,"r",encoding="utf-8")
+    data = yaml.safe_load(f)
+    f.close()
+    return data
+
 def checkmandatory(dicts):
+    if len(dicts) == 0:
+        print("The config is empty!")
+        return {}
+
     modifydicts = dicts.copy()  # Make a copy of the dictionary to avoid modifying the original
     mandatory = ["src", "dst", "address", "type", "mtu", "ttl"]
     # Iterate over each key in the dictionary
     keys_to_remove = []  # List to store keys to remove
     for key, values in modifydicts.items():
         key = key.lower()
-        if key == "global":
-            continue
         missing_keys = []
-        tunnelType = modifydicts[key]["type"]
+        tunnelType = modifydicts.get(key,{}).get("type", None)
+        if tunnelType is None:
+            keys_to_remove.append(key)
+            continue
+        else:
+            tunnelType = tunnelType.lower()
         for item in mandatory:
             if tunnelType == "vxlan" and item == "address":
                 continue
             if item not in values:
                 missing_keys.append(item)
         # If mandatory fields are missing, add this config to remove list
-        src = modifydicts[key].get("src")
-        dst = modifydicts[key].get("dst")
-        ttl = modifydicts[key].get("ttl")
-        mtu = modifydicts[key].get("mtu")
+        src = modifydicts[key].get("src", False)
+        dst = modifydicts[key].get("dst", False)
+        ttl = modifydicts[key].get("ttl", 255)
+        mtu = modifydicts[key].get("mtu", 1450)
         if not testip(src) or not testip(dst):
             print(f"Error: config {key} will not be provisioned because of incorrect src/dst address.")
             keys_to_remove.append(key)
@@ -67,16 +85,6 @@ def checkmandatory(dicts):
         modifydicts.pop(key)
     return modifydicts
 
-def list_sections(conffile):
-    if not os.path.exists(conffile):
-        print("Cannot find specified config file. Exiting...")
-        sys.exit(1)
-
-    config = configparser.ConfigParser()
-    config.read(conffile)
-
-    return config.sections()
-
 def isIPv6(addr):
     ip_for_check = ipaddress.ip_address(addr)
     if isinstance(ip_for_check, ipaddress.IPv6Address):
@@ -84,27 +92,31 @@ def isIPv6(addr):
     else:
         return False
 
-def runCommand(command):
-    print(f"+ Executing command: {command}")
+def runCommand(command, verbose=True):
+    if verbose:
+        print(f"+ Executing command: {command}")
     subprocess.run(command, check=True)
 
 def createTunnel(name,type,localaddr,dstaddr,ttl,mtu,ipaddress):
     cmd = [["ip", "tunnel", "add", f"{type}-{name}", "mode", type, "local", localaddr, "remote", dstaddr, "ttl", ttl],
            ["ip", "link", "set", f"{type}-{name}", "mtu", mtu, "up"],
            ["ip", "addr", "add", ipaddress, "dev", f"{type}-{name}"]]
+    print(f"Creating {type} tunnel {name}...")
     for item in cmd:
-        runCommand(item)
+        runCommand(item, verbose=False)
 
 def creategretap(name,localaddr,dstaddr,ttl,mtu,ipaddress):
     cmd = [["ip", "link", "add", f"gretap-{name}", "type", "gretap", "local", localaddr, "remote", dstaddr, "ttl", ttl],
            ["ip", "link", "set", f"gretap-{name}", "mtu", mtu, "up"],
            ["ip", "addr", "add", ipaddress, "dev", f"gretap-{name}"]]
+    print(f"Creating GRETAP tunnel {name}...")
     for item in cmd:
-        runCommand(item)
+        runCommand(item, verbose=False)
 
 def createLink(name,localaddr,dstaddr,dstport,ttl,vni,mtu,iporbridge):
     cmd = [["ip","link","add",f"vxlan-{name}","type","vxlan","local",localaddr,"remote",dstaddr,"dstport",dstport,"id",vni,"ttl",ttl],
            ["ip","link","set",f"vxlan-{name}","mtu",mtu,"up"]]
+    print(f"Creating vxlan tunnel {name}...")
     if testip(iporbridge):
         cmd.append(["ip","addr","add",iporbridge,"dev",f"vxlan-{name}"])
     else:
@@ -144,161 +156,150 @@ def createConfBak():
         os.remove("/tmp/axtm.conf")
     os.system("cp -r conf.ini /tmp/axtm.conf")
 
-if not detectSth("user"):
-    print("You are not running on user root. Exiting...")
-    sys.exit(1)
+def main():
+    if not detectSth("user"):
+        print("You are not running on user root. Exiting...")
+        sys.exit(1)
 
-confFile = os.path.join(os.getcwd(), "conf.ini")
-sections = list_sections(confFile)
+    parser = argparse.ArgumentParser(description="Specify user-defined config path")
+    parser.add_argument('--config', type=str, default="/opt/axtm/config.yml", help='Config path')
+    args = parser.parse_args()
+    # Detect User-defined Config Path
 
-if not os.path.exists(confFile):
-    print("Error: Cannot find specified config file. Exiting...")
-    sys.exit(1)
+    confFile = args.config
+    if not os.path.exists(confFile):
+        print("Error: Cannot find specified config file. Exiting...")
+        sys.exit(1)
+    sections = readConf(confFile)
 
-config = configparser.ConfigParser()
-config.read(confFile)
-arguments = {}
+    countdown = sections.get("global",{}).get("countdown", 3)
+    try:
+        countdown = float(countdown)
+    except ValueError:
+        print("Warning: Invalid countdown value. Using default (3) seconds.")
+        countdown = 3
 
-countdown = config.get(section="global", option="countdown", fallback="3")
-try:
-    countdown = float(countdown)
-except ValueError:
-    print("Warning: Invalid countdown value. Using default (3) seconds.")
-    countdown = 3
+    createConfBak()
+    print(f"Welcome. AXTM will start provision tunnel(s) after {countdown} seconds....")
+    time.sleep(countdown)
 
-createConfBak()
-print(f"Welcome. AXTM will start provision tunnel(s) after {countdown} seconds....")
-time.sleep(countdown)
+    checkedArgs = checkmandatory(sections.get("configs",{}))
 
-for item in sections:
-    name = item.lower()
-    if len(name) > 6:
-        name = name[0:6]
-    if not name.isalpha():
-        name = name[0:6] + "a"
-    if name not in arguments:
-        arguments[name] = {}
-    for key, value in config[item].items():
-        key = key.lower()
-        arguments[name][key] = value
+    if len(checkedArgs) == 0:
+        print("There is no valid tunnel configuration in conf.ini. Exiting...")
+        sys.exit(1)
 
-checkedArgs = checkmandatory(arguments)
+    for name, conf in checkedArgs.items():
+        name = name.lower()
+        types = checkedArgs[name]["type"]
 
-if not checkedArgs:
-    print("There is no valid tunnel configuration in conf.ini. Exiting...")
-    sys.exit(1)
-
-for name, conf in checkedArgs.items():
-    name = name.lower()
-
-    if name == "global":
-        continue
-    # Skip global configuration
-
-    types = checkedArgs[name]["type"]
-
-    if detectSth("tunnel",f"{types}-{name}"):
-        print(f"The tunnel {name} already up. Skipped.")
-        continue
-
-    if conf["type"] == "vxlan":
-        vni = conf.get("vni")
-        dstport = conf.get("dstport")
-
-        if not vni or not dstport:
-            print(f"Incomplete configuration for vxlan {name}. Skipped.")
+        if detectSth("tunnel",f"{types}-{name}"):
+            print(f"The tunnel {name} already up. Skipped.")
             continue
 
-        if not checkvalue("dstport", dstport):
-            print(f"The config {name} contains invalid dstport. Skipping...")
-            continue
+        if conf["type"] == "vxlan":
+            if "vni" in conf:
+                vni = conf.get("vni")
+            elif "id" in conf:
+                vni = conf.get("id")
+            else:
+                print("Unknown vxlan ID. Skipping...")
+                continue
+            dstport = conf.get("dstport", False)
 
-        if "bridge" in conf and conf["bridge"]:
-            if detectSth("bridge",conf["bridge"]):
-                try:
-                    if "preup" in conf:
-                        prepostup("preup",conf["preup"])
-                except Exception as e:
-                    print(f"Error executing preup command for tunnel {name}: {e}")
-                    continue
-                createLink(name, conf["src"], conf["dst"], conf["dstport"],
-                           conf["ttl"], conf["vni"], conf["mtu"], conf["bridge"])
-                try:
-                    if "postup" in conf:
-                        prepostup("postup",conf["postup"])
-                except Exception as e:
-                    print(f"Error executing postup command for tunnel {name}: {e}")
+            if not dstport or not checkvalue("dstport", dstport):
+                print(f"Incomplete configuration for vxlan {name}. Skipped.")
+                continue
+
+            if "bridge" in conf and conf["bridge"]:
+                if detectSth("bridge",conf["bridge"]):
+                    try:
+                        if "preup" in conf["pre_post_scripts"]:
+                            prepostup("preup",conf.get("pre_post_scripts",{}).get("preup",[]))
+                    except Exception as e:
+                        print(f"Error executing preup command for tunnel {name}: {e}")
+                        continue
+                    createLink(name, conf["src"], conf["dst"], conf["dstport"],
+                               conf["ttl"], vni, conf["mtu"], conf["bridge"])
+                    try:
+                        if "postup" in conf["pre_post_scripts"]:
+                            prepostup("postup",conf.get("pre_post_scripts",{}).get("postup",[]))
+                    except Exception as e:
+                        print(f"Error executing postup command for tunnel {name}: {e}")
+                        continue
+                else:
+                    print(f"Bridge {conf['bridge']} does not exist. Skipping...")
                     continue
             else:
-                print(f"Bridge {conf['bridge']} does not exist. Skipping...")
-                continue
-        else:
+                if testip(conf["address"]):
+                    try:
+                        if "preup" in conf["pre_post_scripts"]:
+                            prepostup("preup",conf.get("pre_post_scripts",{}).get("preup",[]))
+                    except Exception as e:
+                        print(f"Error executing preup command for tunnel {name}: {e}")
+                        continue
+                    createLink(name, conf["src"], conf["dst"], conf["dstport"],
+                               conf["ttl"], vni, conf["mtu"], conf["address"])
+                    try:
+                        if "postup" in conf["pre_post_scripts"]:
+                            prepostup("postup",conf.get("pre_post_scripts",{}).get("postup",[]))
+                    except Exception as e:
+                        print(f"Error executing postup command for tunnel {name}: {e}")
+                        continue
+                else:
+                    print(f"Incorrect endpoint address. Skipping config {name}...")
+                    continue
+        elif conf["type"] == "gretap":
             if testip(conf["address"]):
                 try:
-                    if "preup" in conf:
-                        prepostup("preup",conf["preup"])
+                    if "preup" in conf["pre_post_scripts"]:
+                        prepostup("preup",conf.get("pre_post_scripts",{}).get("preup",[]))
                 except Exception as e:
                     print(f"Error executing preup command for tunnel {name}: {e}")
                     continue
-                createLink(name, conf["src"], conf["dst"], conf["dstport"],
-                           conf["ttl"], conf["vni"], conf["mtu"], conf["address"])
+                creategretap(name,conf["src"],conf["dst"],conf["ttl"],conf["mtu"],conf["address"])
                 try:
-                    if "postup" in conf:
-                        prepostup("postup",conf["postup"])
+                    if "postup" in conf["pre_post_scripts"]:
+                        prepostup("postup",conf.get("pre_post_scripts",{}).get("postup",[]))
                 except Exception as e:
                     print(f"Error executing postup command for tunnel {name}: {e}")
                     continue
             else:
                 print(f"Incorrect endpoint address. Skipping config {name}...")
                 continue
-    elif conf["type"] == "gretap":
-        if testip(conf["address"]):
-            try:
-                if "preup" in conf:
-                    prepostup("preup",conf["preup"])
-            except Exception as e:
-                print(f"Error executing preup command for tunnel {name}: {e}")
-                continue
-            creategretap(name,conf["src"],conf["dst"],conf["ttl"],conf["mtu"],conf["address"])
-            try:
-                if "postup" in conf:
-                    prepostup("postup",conf["postup"])
-            except Exception as e:
-                print(f"Error executing postup command for tunnel {name}: {e}")
-                continue
         else:
-            print(f"Incorrect endpoint address. Skipping config {name}...")
-            continue
-    else:
-        # Non-vxlan tunnel
-        if conf["type"] == "sit":
-            try:
-                src_ip = ipaddress.ip_address(conf["src"].split("/")[0])
-                dst_ip = ipaddress.ip_address(conf["dst"].split("/")[0])
-                if isinstance(src_ip, ipaddress.IPv6Address) or isinstance(dst_ip, ipaddress.IPv6Address):
-                    print(f"Error: sit tunnel {name} is not allowed to use IPv6 as src/dst.")
+            # Non-vxlan tunnel
+            if conf["type"] == "sit":
+                try:
+                    src_ip = ipaddress.ip_address(conf["src"].split("/")[0])
+                    dst_ip = ipaddress.ip_address(conf["dst"].split("/")[0])
+                    if isinstance(src_ip, ipaddress.IPv6Address) or isinstance(dst_ip, ipaddress.IPv6Address):
+                        print(f"Error: sit tunnel {name} is not allowed to use IPv6 as src/dst.")
+                        continue
+                except ValueError:
+                    print(f"Invalid IP format in config {name}. Skipping...")
                     continue
-            except ValueError:
-                print(f"Invalid IP format in config {name}. Skipping...")
+
+            if testip(conf["address"]):
+                try:
+                    if "preup" in conf["pre_post_scripts"]:
+                        prepostup("preup",conf.get("pre_post_scripts",{}).get("preup",[]))
+                except Exception as e:
+                    print(f"Error executing preup command for tunnel {name}: {e}")
+                    continue
+                createTunnel(name, conf["type"], conf["src"], conf["dst"],
+                             conf["ttl"], conf["mtu"], conf["address"])
+                try:
+                    if "postup" in conf["pre_post_scripts"]:
+                        prepostup("postup",conf.get("pre_post_scripts",{}).get("postup",[]))
+                except Exception as e:
+                    print(f"Error executing postup command for tunnel {name}: {e}")
+                    continue
+            else:
+                print(f"Incorrect endpoint address. Skipping config {name}...")
                 continue
 
-        if testip(conf["address"]):
-            try:
-                if "preup" in conf:
-                    prepostup("preup",conf["preup"])
-            except Exception as e:
-                print(f"Error executing preup command for tunnel {name}: {e}")
-                continue
-            createTunnel(name, conf["type"], conf["src"], conf["dst"],
-                         conf["ttl"], conf["mtu"], conf["address"])
-            try:
-                if "postup" in conf:
-                    prepostup("postup",conf["postup"])
-            except Exception as e:
-                print(f"Error executing postup command for tunnel {name}: {e}")
-                continue
-        else:
-            print(f"Incorrect endpoint address. Skipping config {name}...")
-            continue
+    print(f"Process completed.")
 
-print(f"Process completed.")
+if __name__ == "__main__":
+    main()
